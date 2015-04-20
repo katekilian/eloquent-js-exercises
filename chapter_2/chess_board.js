6fda54d82756da16928d6cf733ff91a5d7a16c1a
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-19 23:33:57 -0600

    Refactored first solution, completed any-number-sized grid answer

--- chapter_2/chess_board.js
+++ chapter_2/chess_board.js
@@ -1,19 +1,42 @@
-var chessBoard = "# # # # \n # # # #\n";
+// basic chess board
+
+var chessBoard = "# # \n # #\n";
 for (counter = 1; counter <= 4; counter++)
-  complete = chessBoard.repeat(counter);
+  complete = chessBoard.repeat(counter / 2);
 console.log(complete);
 
-// flexible version
+// only works for even numbered grid sizes
 
 var twoSpaces = "# ";
-grid = prompt("What size would you like your grid to be?", "");
-numColumns = twoSpaces.repeat(grid / 2);
+grid1 = prompt("What size would you like your grid to be?", "");
+numColumns = twoSpaces.repeat(grid1 / 2);
 numRows = "";
-for (counter = 1; counter <= grid; counter += 1) {
+for (counter = 1; counter <= grid1; counter += 1) {
   if (counter % 2 === 0) {
     numRows = numRows + (" " + numColumns + "\n");
   } else if (counter % 2 === 1) {
       numRows = numRows + (numColumns + "\n");
+  }
+}
+console.log(numRows);
+
+// solution for any-numbered grid sizes
+
+grid2 = prompt("What size would you like your grid to be?", "");
+row = "";
+for (counter = 1; counter <= grid2; counter += 1) {
+  if (counter % 2 === 1) {
+    row = row + "#";
+  } else if (counter % 2 === 0) {
+    row = row + " ";
+  }
+}
+numRows = "";
+for (counter = 1; counter <= grid2; counter += 1) {
+  if (counter % 2 === 0) {
+    numRows = numRows + (" " + row + "\n");
+  } else if (counter % 2 === 1) {
+      numRows = numRows + (row + "\n");
   }
 }
 console.log(numRows);
