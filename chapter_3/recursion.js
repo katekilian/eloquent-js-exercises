91239a7bb755bd3b4de36a30482dc23ffe45905c
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-22 09:47:06 -0600

    Added if statement to handle negative numbers

--- chapter_3/recursion.js
+++ chapter_3/recursion.js
@@ -1,6 +1,9 @@
 // a working answer (except negative case) without recursion...
 
 var isEven = function(num) {
+  if (num < 0) {
+    num = num * -1
+  }
   if (num % 2 === 0) {
     return true;
   } else if (num % 2 === 1) {
@@ -19,6 +22,9 @@ console.log(isEven(-1));
 // trying to use recursion?
 
 var isEven = function(num) {
+  if (num < 0) {
+    num = num * -1
+  }
   if (num % 2 === 0) {
     return true;
   } else if (num % 2 === 1) {
