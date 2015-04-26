108dcae7afab739c7fa01519ed44742e06ae183f
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-26 17:33:56 -0600

    Finished Ch 4 reversing array part 2

--- chapter_4/reversing_array.js
+++ chapter_4/reversing_array.js
@@ -1,9 +1,7 @@
 // jsbin url: http://jsbin.com/jexujas/edit
 
-// reversing an array through outputting a new array:
-
-var arr = ["a","b","c","d","e"];
-
+var arrOdd = ["a","b","c","d","e","f","g"];
+var arrEven = ["q","r","s","t"];
 
 var reverseArray = function(array) {
   var result = [];
@@ -13,27 +11,22 @@ var reverseArray = function(array) {
   return result;
 };
 
-console.log(reverseArray(arr));
+console.log(reverseArray(arrOdd));
+console.log(reverseArray(arrEven));
 
-// reversing an array in place:
+// using a tmp variable to hold the value of the places that are being swapped.
 
 var arrayReverseInPlace = function(array) {
   var tmp = "";
   var len = array.length - 1;
-  for (i = 0; i <= array.length - 1; i++) {
+  for (i = 0; i < ((array.length)/2); i++) {
     tmp = array[i];
-      console.log("I was array[i]: " + tmp);
-      console.log("I'm the index that array[i] is swapping with: " + len);
     array[i] = array[len];
-      console.log("array[i] is now " + array[i]);
     array[len] = tmp;
-      console.log("array len is now " + array[len]);
     len--;
-      console.log("-----------");
   }
   return array;
 };
 
-console.log(arrayReverseInPlace(arr));
-
-// Current issue for ReverseInPlace: if "return array;" is inside the loop, it only prints the outcome of one loop (of course) but you can see that the first and last indices have swapped.  If you move "return array;" OUTSIDE the loop but still inside the function, the output is the original, unswapped array... and I cannot figure out why for the life of me.
+console.log(arrayReverseInPlace(arrOdd));
+console.log(arrayReverseInPlace(arrEven));
