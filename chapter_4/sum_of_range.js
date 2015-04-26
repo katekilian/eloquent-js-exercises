ff8a010c9f6934c9df16f52d9b66508499e44b04
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-26 15:09:01 -0600

    Completed Chapter 4 sum of range exercise

--- chapter_4/sum_of_range.js
+++ chapter_4/sum_of_range.js
@@ -1,9 +1,26 @@
-rangeArray = []
+// jsbin url: http://jsbin.com/hobadi/edit?js,console
+
+var rangeArray = [];
+
 var range = function(start, end) {
   for (var i = start; i <= end; i++) {
     rangeArray.push(i);
   }
   return rangeArray;
-}
+};
 
-range(1,10);
+console.log(range(1,10));
+
+var sumOfRange = function(array) {
+  sum = "";
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === array[0]) {
+      sum = array[i];
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfRange(rangeArray));
