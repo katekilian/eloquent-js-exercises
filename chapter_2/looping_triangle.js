56e44bea9566630ea35162de9185294932966519
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-25 19:54:15 -0600

    Finally saving correctly refactored version of looping triangle

--- chapter_2/looping_triangle.js
+++ chapter_2/looping_triangle.js
@@ -1,31 +1,15 @@
+// jsbin url: http://jsbin.com/linidi/edit?js,console
+
 result = "";
 for (var counter = 0; counter <= 7; ++counter)
   console.log(result = result + "#");
 
-// solution without re-printing to console.log every time.
-// my goal is to build up a string wiht the appropriate amounts of #s, and with line breaks added after every time that the row is incremented upon.
+// refactored to accumulate the triangle inside result before printing to console
 
-
-result = "";
-row = "";
+var result = "";
+var row = "";
 for (var i = 0; i < 7; i++) {
   row = "#".repeat(i + 1);
-  result += (row + "\n");
+  result = result + (row + "\n");
 }
-return row;
-
-// my jsbin that has what I'm trying: http://jsbin.com/kebudi/1/edit
-
-
-
-
-
-
-// Refactored using .length
-// result = "";
-// for (var counter = 0; counter <= 7; ++counter)
-//   console.log(result = result + "#");
-//
-//   if (i !== 7)
-//     result += "\n"
-// then print result to the console once
+console.log(result);
